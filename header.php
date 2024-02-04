b1cccb39593c7ffb70d86b1168ddaf801339010e
Cuong Le <tekielee@yahoo.com>
2024-02-03 19:13:48 -0800

    home hero

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 
 <body <?php body_class(); ?>>
 
-<div class="container-fluid">
+<div class="container-fluid p-0">
 
     <?php
 
@@ -47,5 +47,44 @@
 
         }
 
+    ?>
+
+    <?php
+
+        // get background image from the admin
+
+        // get hero title from the admin
+
+        // get hero text from the admin
+
+        // get the button text from the admin
 
     ?>
+
+    <div class="bg-white text-secondary px-4 py-5 text-center"
+
+        style="background-image:url( '<?php echo get_template_directory_uri()?>/images/home-hero.jpg' );
+
+        background-repeat: no-repeat; background-position: center;"
+
+    >
+
+        <div class="py-5">
+
+            <h1 class="display-5 fw-bold text-black">Dark color hero</h1>
+
+            <div class="col-lg-6 mx-auto">
+
+                <p class="fs-5 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+
+                <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+
+                    <button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Shopping</button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
--- template-parts/navbar-simple.php
+++ template-parts/navbar-simple.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 
-    <div class="container-fluid">
+    <div class="container-fluid pb-3">
 
         <a class="navbar-brand" href="#">Navbar scroll</a>
 
@@ -12,7 +12,7 @@
 
         <div class="collapse navbar-collapse" id="navbarScroll">
 
-            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+            <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
 
                 <?php
 

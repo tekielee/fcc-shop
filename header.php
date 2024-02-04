5d07846ad3f730daa7a462d1dc9b743cb2a679e8
Cuong Le <tekielee@yahoo.com>
2024-02-03 17:16:38 -0800

    add woocomerce files

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 
         <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
 
-    	  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 
         <meta name="description" content="<?php echo bloginfo('description');?>" />
 
@@ -25,9 +25,9 @@
 
         <script src="<?php echo get_template_directory_uri() . '/assets/js/site.js'?>"></script>
 
-    	  <?php wp_head(); ?>
+    	<?php wp_head(); ?>
 
-        </head>
+    </head>
 
 <body <?php body_class(); ?>>
 

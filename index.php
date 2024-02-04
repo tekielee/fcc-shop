8f989fef3b795eefe7867645bb509d4c49fe34c8
Cuong Le <tekielee@yahoo.com>
2024-02-03 16:27:48 -0800

    add woocomerce files

--- index.php
+++ index.php
@@ -2,15 +2,7 @@
 
 <div class="container-fluid">
 
-  <?php
-
-    do_action( 'woocommerce_register_form' );
-
-    do_action( 'woocommerce_before_checkout_form');
-
-    do_action( 'woocommerce_account_navigation' );
-
-  ?>
+    <?php ?>
 
 </div>
 
---
+++ woocommerce/cart.php
@@ -0,0 +1,9 @@
+<?php
+
+    do_action( 'woocommerce_register_form' );
+
+    do_action( 'woocommerce_before_checkout_form');
+
+    do_action( 'woocommerce_account_navigation' );
+
+?>

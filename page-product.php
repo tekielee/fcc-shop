f289e7b31566808d7d7fa5dd9fe94eabf7c828e4
Cuong Le <tekielee@yahoo.com>
2024-02-14 11:10:54 -0800

    specs and profiles and similar products

--- page-product.php
+++ page-product.php
@@ -209,6 +209,115 @@
 
     </div>
 
+    <table class="product-related-table">
+
+        <tr>
+
+            <td>
+
+                <ul class="nav">
+
+                    <li class="nav-item">
+
+                        <a id="spec" class="nav-link active" aria-current="page" href="javascript:void(0)">SPECIFICATION</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a id="warranty" class="nav-link" href="javascript:void(0)">WARRANTY INFO</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a id="shipping" class="nav-link" href="javascript:void(0)">SHIPPING INFO</a>
+
+                    </li>
+
+                    <li class="nav-item">
+
+                        <a id="seller" class="nav-link" href="javascript:void(0)">SELLER PROFILE</a>
+
+                    </li>
+
+                </ul>
+
+            </td>
+
+            <td>
+
+                <h4>Similar Products</h4>
+
+                <div class="list-group">
+
+                    <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+
+                    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+
+                        <div class="d-flex gap-2 w-100 justify-content-between">
+
+                            <div>
+
+                                <h6 class="mb-0">List group item heading</h6>
+
+                                <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+
+                            </div>
+
+                            <small class="opacity-50 text-nowrap">now</small>
+
+                        </div>
+                    </a>
+
+                    <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+
+                    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+
+                        <div class="d-flex gap-2 w-100 justify-content-between">
+
+                            <div>
+
+                                <h6 class="mb-0">Another title here</h6>
+
+                                <p class="mb-0 opacity-75">Some placeholder content in a paragraph that goes a little longer so it wraps to a new line.</p>
+
+                            </div>
+
+                            <small class="opacity-50 text-nowrap">3d</small>
+
+                        </div>
+
+                    </a>
+
+                    <a href="#" class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+
+                    <img src="https://github.com/twbs.png" alt="twbs" width="32" height="32" class="rounded-circle flex-shrink-0">
+
+                        <div class="d-flex gap-2 w-100 justify-content-between">
+
+                            <div>
+
+                                <h6 class="mb-0">Third heading</h6>
+
+                                    <p class="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+
+                            </div>
+
+                            <small class="opacity-50 text-nowrap">1w</small>
+
+                        </div>
+
+                    </a>
+
+                </div>
+
+            </td>
+
+        </tr>
+
+    </table>
+
 </div>
 
 <?php get_footer(); ?>

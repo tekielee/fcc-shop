9eeba71df427bc8fafaff96c366f963aa18a44e9
Cuong Le <tekielee@yahoo.com>
2024-02-17 00:08:36 -0800

    register primary menu

--- functions.php
+++ functions.php
@@ -6,7 +6,15 @@ if ( ! function_exists ( 'register_my_menu' ) ) {
 
     function register_my_menu () {
 
-        register_nav_menu ( 'primary', __( 'Primary Menu', 'bootstrap-woocommerce' ) );
+        register_nav_menu (
+            
+            array (
+
+                'primary-menu' => 'Primary Menu'
+
+            )
+
+        );
 
     }
 

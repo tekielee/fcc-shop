b390ffe94241178da05e8bb65715b4d017988d02
Cuong Le <tekielee@yahoo.com>
2024-03-08 12:25:30 -0800

    add subscriber form

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
 <div class="grid-x footer">
 
-    <div class="large-6 cell">
+    <div class="large-3 cell">
 
         <?php
 
@@ -16,18 +16,81 @@
 
     </div>
 
-    <div class="large-6 cell">
+    <div class="large-7 cell">
 
-        <?php
+        <div class="grid-x">
 
-            _e ( wp_unslash ( get_option ( 'youtube_url' ) ) );
+            <div class="large-12 cell">
 
-            _e ( wp_unslash ( get_option ( 'twitter_url' ) ) );
+                <div class="grid-x">
 
-            _e ( wp_unslash ( get_option ( 'instagram_url' ) ) );
+                    <div class="large-2 cell"></div>
 
+                    <div class="large-6 cell">
 
-        ?>
+                        <div class="grid-x">
+                        
+                            <div class="large-10 cell">
+
+                                <input type="text" placeholder="Subscriber Email Address" id="subscriber" />
+
+                            </div>
+
+                            <div class="large-2 cell">
+
+                                <button id="save-subscriber" class="submit success button">Subscribe</button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="large-4 cell"></div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="large-2 cell">
+
+        <div class="grid-x social-icons-margin">
+
+            <div class="large-4 cell">
+
+                <a href="<?php _e ( wp_unslash ( get_option ( 'youtube_url' ) ) ); ?>" target="_blank">
+                
+                    <i class="fi-social-youtube social-icon"></i>
+
+                </a>
+
+            </div>
+
+            <div class="large-4 cell">
+
+                <a href="<?php _e ( wp_unslash ( get_option ( 'twitter_url' ) ) ); ?>" target="_blank">
+                
+                    <i class="fi-social-twitter social-icon"></i>
+
+                </a>
+
+            </div>
+
+            <div class="large-4 cell">
+
+                <a href="<?php _e ( wp_unslash ( get_option ( 'instagram_url' ) ) ); ?>" target="_blank">
+                
+                    <i class="fi-social-instagram social-icon"></i>
+
+                </a>
+
+            </div>
+
+        </div>
 
     </div>
 
--- header.php
+++ header.php
@@ -13,6 +13,8 @@
 
         <link rel="stylesheet" href="<?php echo get_template_directory_uri () . '/css/foundation.min.css' ?>" />
 
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri () . '/css/foundation-icons.css' ?>" />
+
         <link rel="stylesheet" href="<?php echo get_template_directory_uri () . '/css/app.css' ?>" />
 
     	<?php wp_head (); ?>
